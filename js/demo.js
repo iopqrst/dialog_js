b4b19ae411f5b63eed75d3cc072f73bcdea631d4
iopqrst <opqrst@163.com>
2014-10-16 17:52:28 +0800

    #提交了！！！！

--- js/demo.js
+++ js/demo.js
@@ -4,4 +4,12 @@ define(function(require, exports, module) {
 	// 在增加两行，看看有没有变化！
 	
 	// 测试从本地pull 文件看看是不是能把这个文件从服务器上拉下来。
+	
+	/**
+	 *  修复bug 100
+	 */
+	require('bug100.js');
+	function fixedBug100() {
+		alert('修复bug100');
+	}
 });
